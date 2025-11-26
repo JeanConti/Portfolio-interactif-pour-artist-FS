7761d4b00492ffa37f53e7bc978768afa4c225a3
Jean-Michel Conti <114805482+JeanConti@users.noreply.github.com>
2025-11-26 08:43:40 +0100

    feat: Establish Express backend with EJS views and initial portfolio page structure.

--- 02 back-end/src/server.js
+++ 02 back-end/src/server.js
@@ -12,35 +12,46 @@ server.set('view options', {delimiter: '?'})
 server.set('views', path.join(__dirname, '../src/views'))
 
 
+
+server.use(express.static('../../01 front-end/public'))
+
+
 // Routes
 server.use('/', routePages)
+
+server.get('/', (req, res) => {
+  res.render('Home')
+})
+
 
 
 server.use(express.json)
 
 
 // Récupérer les oeuvres
-server.get('/api/artworks', (req, res) => {
+/* server.get('/api/artworks', (req, res) => {
   const artworks = prisma.artworks.findMany()
   res.json(artworks)
-})
+}) */
 
 // Ajouter une oeuvre
-server.post('/api/artworks', (req, res) => {
+/* server.post('/api/artworks', (req, res) => {
   const {titre, annee, categorie, imageUrl} = req.body
   const newArtwork = prisma.artwork.create({
     data: {titre, annee, categorie, imageUrl},
   })
   res.json(newArtwork)
-})
+}) */
 
 
 // Suprimer une oeuvre
-server.delete('/api/artworks', (req, res) => {
+/* server.delete('/api/artworks', (req, res) => {
   const {titre, annee, categorie, imageUrl} = req.body
   const deleteArtwork = prisma.artwork.delete({
     data: {titre, annee, categorie, imageUrl},
   })
   res.json(newArtwork)
-})
+}) */
+
+
 server.listen(port, () => console.log(`Connectée au port: ${port}!`))
